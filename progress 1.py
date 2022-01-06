8a6a5140a18904d5d16c9cde537bbb7b45aad92d
sally-gif <72941121+sally-gif@users.noreply.github.com>
2022-01-06 01:53:20 -0800

    Add files via upload

---
+++ progress 1.py
@@ -0,0 +1,18 @@
+print('WELCOME TO THE ROLLERCOSTER')
+height=int(input('what is your height? '))
+if height >120:
+    print('input your age') 
+    age=int(input('what is your age? '))
+    if age <= 18:
+       print('please pay $5 and enjoy the ride')
+    else:
+       print(' please pay $7 and enjoy the ride')
+#print ('enjoy the ride')       
+else:
+    sorry=int(input('your age?'))
+    if sorry<= 10:
+        print('have an ice cream')
+    else:
+        print ("go home")
+    print('grow taller')
+print('BYE!!!')         
