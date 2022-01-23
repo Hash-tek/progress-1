fd28290319b488b0e5753c74767aaa48513aac92
sally-gif <72941121+sally-gif@users.noreply.github.com>
2022-01-23 04:52:44 -0800

    Update pizza order.py

--- pizza order.py
+++ pizza order.py
@@ -1,6 +1,6 @@
 print('welcome to sally pizza')
 bill=0
-order=input('what size pizza do you want? S,M,L:  ')
+order=input('what size pizza do you want? S,M,L:  ').upper()
 if order == 'S':
     bill = 15
     print ('it will cost you $15')
@@ -10,11 +10,11 @@ elif order =='M':
 else:
     bill =25
     print ('it will cost you $25')
-add_cheese=input('do you want cheese? Y OR N: ')
+add_cheese=input('do you want cheese? Y OR N: ').upper()
 if add_cheese=='Y':
     bill+=1
     print ('added')
-add_pepperoni=input('do you want pepperoni? S OR ML: ')
+add_pepperoni=input('do you want pepperoni? S OR ML: ').upper()
 if add_pepperoni == "S":
     bill+=2
     print('it will cost you $2')
