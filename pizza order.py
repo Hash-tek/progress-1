14ed103a1f1f98c1465832ca8f684b6cc035888c
sally-gif <72941121+sally-gif@users.noreply.github.com>
2022-01-23 12:45:00 +0100

    Add files via upload
    first code without  udemy :)

---
+++ pizza order.py
@@ -0,0 +1,26 @@
+print('welcome to sally pizza')
+bill=0
+order=input('what size pizza do you want? S,M,L:  ')
+if order == 'S':
+    bill = 15
+    print ('it will cost you $15')
+elif order =='M':
+    bill=20
+    print ('it will cost you $20')
+else:
+    bill =25
+    print ('it will cost you $25')
+add_cheese=input('do you want cheese? Y OR N: ')
+if add_cheese=='Y':
+    bill+=1
+    print ('added')
+add_pepperoni=input('do you want pepperoni? S OR ML: ')
+if add_pepperoni == "S":
+    bill+=2
+    print('it will cost you $2')
+else:
+    bill+=3
+    print ('it will cost you $3') 
+print (f'your bills is ${bill}')              
+
+
